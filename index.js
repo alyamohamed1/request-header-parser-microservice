a42ec9c1ff4b5d9c378ffe0bd0f26456ab4cdc15
alyamohamed1 <alya.altawash@gmail.com>
2025-11-06 21:58:37 +0300

    Complete request header parser microservice

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // index.js
-// where your node app starts
+// Request Header Parser Microservice
 
-// init project
 require('dotenv').config();
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,9 +18,25 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// ✅ Main project endpoint
+app.get('/api/whoami', function (req, res) {
+  // IP address
+  var ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.connection.remoteAddress;
+  if (ipaddress && ipaddress.indexOf(',') !== -1) {
+    ipaddress = ipaddress.split(',')[0]; // take first IP if multiple
+  }
+
+  // Preferred language
+  var language = req.headers['accept-language'];
+
+  // Software (user agent)
+  var software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
