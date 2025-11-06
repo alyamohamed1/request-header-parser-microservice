0a2194805c71239ec597c0cfd9db140e90961297
alyamohamed1 <alya.altawash@gmail.com>
2025-11-06 22:08:16 +0300

    Fix /api/whoami response

--- index.js
+++ index.js
@@ -7,35 +7,31 @@ var cors = require('cors');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
+// serve static files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// root route
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// ✅ Main project endpoint
+// ✅ MAIN PROJECT ROUTE
 app.get('/api/whoami', function (req, res) {
-  // IP address
-  var ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.connection.remoteAddress;
-  if (ipaddress && ipaddress.indexOf(',') !== -1) {
-    ipaddress = ipaddress.split(',')[0]; // take first IP if multiple
+  // IP address: first check x-forwarded-for (when behind a proxy), fall back to req.ip
+  var ip = req.headers['x-forwarded-for'] || req.ip;
+  if (ip && ip.indexOf(',') !== -1) {
+    ip = ip.split(',')[0]; // take first IP if list
   }
 
-  // Preferred language
   var language = req.headers['accept-language'];
-
-  // Software (user agent)
   var software = req.headers['user-agent'];
 
   res.json({
-    ipaddress: ipaddress,
+    ipaddress: ip,
     language: language,
-    software: software,
+    software: software
   });
 });
 
@@ -43,3 +39,4 @@ app.get('/api/whoami', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
